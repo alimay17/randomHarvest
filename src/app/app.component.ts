6b672a1a7f2c58ef23ca7c8022ca2c055cf3efd3
Aly Smith <alimay.smith@outlook.com>
2023-01-02 13:39:58 -0700

    updating author, date, class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  author = 'Alice Smith';
+  class = 'WDD 430: Prof. Murff'
   title = 'Random Harvest';
+  date = new Date().toLocaleString(
+    'default', {month: 'short', year: 'numeric'}
+  );
 }
