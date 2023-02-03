17e1c365ad590264d66b1aa4958f64f8203221ef
Alice Smith <alimay.smith@outlook.com>
2023-02-02 19:48:38 -0700

    implement initial slService function

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,23 @@ import { Injectable } from '@angular/core';
 export class ShoppingListService {
 
   constructor() { }
+
+  // events
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+
+  // properties
+  private ingredients: Ingredient[] = [
+    new Ingredient('Rice', 1),
+    new Ingredient('Spice', 7)
+  ];
+
+  // methods
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
 }
