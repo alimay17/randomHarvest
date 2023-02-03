85dae5111eeeea1fad083f4446515c088c3983b1
Alice Smith <alimay.smith@outlook.com>
2023-02-02 19:56:19 -0700

    added ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 @Injectable({
@@ -10,8 +11,18 @@ export class RecipeService {
 
   // properties
   private recipes: Recipe[] = [
-    new Recipe('Jollof Rice', 'Most Excellent Rice Dish', 'https://i0.wp.com/www.nigerianlazychef.com/wp-content/uploads/2015/05/IMG_2943-2.jpg'),
-    new Recipe('Jollof Rice2', 'Most Excellent Rice Dish', 'https://i0.wp.com/www.nigerianlazychef.com/wp-content/uploads/2015/05/IMG_2943-2.jpg')
+    new Recipe(
+      'Jollof Rice', 
+      'Most Excellent Rice Dish', 
+      'https://i0.wp.com/www.nigerianlazychef.com/wp-content/uploads/2015/05/IMG_2943-2.jpg',
+      [new Ingredient('rice', 12)]
+      ),
+    new Recipe(
+      'Jollof Rice2', 
+      'Most Excellent Rice Dish', 
+      'https://i0.wp.com/www.nigerianlazychef.com/wp-content/uploads/2015/05/IMG_2943-2.jpg',
+      [new Ingredient('beans', 12)]
+      )
   ];
   recipeSelected = new EventEmitter<Recipe>();
 
