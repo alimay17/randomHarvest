0a635ad6ca2016da5b308737f9e9dd8e6076100f
Alice Smith <alimay.smith@outlook.com>
2023-02-20 14:59:21 -0700

    implement form controls & validate

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 @Component({
@@ -7,19 +8,16 @@ import { ShoppingListService } from '../shopping-list.service';
   styleUrls: ['./shopping-list-edit.component.css']
 })
 export class ShoppingListEditComponent {
+  
   // constructors
   constructor(private slService: ShoppingListService){}
 
-  // properties
-  @ViewChild('nameInput') nameInputRef!: ElementRef;
-  @ViewChild('amountInput') amountInputRef!: ElementRef;
-
   // methods
-  onAddItem(){
-    const iName = this.nameInputRef.nativeElement.value;
-    const iAmount = this.amountInputRef.nativeElement.value;
-    const newIngredient = new Ingredient(iName, iAmount);
+  onAddItem(form: NgForm){
+    const value = form.value;
+    const newIngredient = new Ingredient(value.name, value.amount);
 
     this.slService.addIngredient(newIngredient);
+    form.reset();
   }
 }
