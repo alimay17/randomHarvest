97b6bb540c56a2be597dcb9d2529a4ccf7a1635e
Alice Smith <alimay.smith@outlook.com>
2023-02-20 16:42:54 -0700

    implement edit shoppinglist

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
+
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 @Component({
@@ -7,17 +9,51 @@ import { ShoppingListService } from '../shopping-list.service';
   templateUrl: './shopping-list-edit.component.html',
   styleUrls: ['./shopping-list-edit.component.css']
 })
-export class ShoppingListEditComponent {
+export class ShoppingListEditComponent implements OnInit, OnDestroy{
+
+  // properties
+  subscription!: Subscription;
+  editMode = false;
+  itemIndex!: number;
+  editedItem!: Ingredient;
+
+  // form
+  @ViewChild('f') slForm!: NgForm;
   
   // constructors
   constructor(private slService: ShoppingListService){}
+
+  // implements
+  ngOnInit(): void {
+    this.subscription = this.slService.startedEditing.subscribe(
+      (index: number) => {
+        this.itemIndex = index;
+        this.editMode = true;
+        this.editedItem = this.slService.getIngredient(index);
+
+        // load ingredient to form
+        this.slForm.setValue({
+          name: this.editedItem.name,
+          amount: this.editedItem.amount
+        });
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 
   // methods
   onAddItem(form: NgForm){
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
 
+    if(this.editMode) {
+      this.slService.updateIngredient(this.itemIndex, newIngredient);
+    } else {
     this.slService.addIngredient(newIngredient);
+    }
+    this.editMode = false;
     form.reset();
   }
 }
