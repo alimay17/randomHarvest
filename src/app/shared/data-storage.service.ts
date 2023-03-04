36b43af18c8915903973f2c7e3cd8ccadb9fe98c
Alice Smith <alimay.smith@outlook.com>
2023-03-04 13:08:27 -0700

    initial data-storage service

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataStorageService {
+
+  // properties
+  urlBase = ''
+
+  // constructor
+  constructor() { }
+
+  // methods
+  getAllData() {
+
+  }
+
+  getItem() {
+
+  }
+
+  postItem(){
+
+  }
+
+  deleteItem(){
+    
+  }
+
+  deleteAllData(){
+    
+  }
+}
